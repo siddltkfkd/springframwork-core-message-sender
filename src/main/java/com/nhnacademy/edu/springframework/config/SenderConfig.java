6602f2d41ffbe6a27dc6b8cb71cc2372e94312ca
siddltkfkd <unji2000@gmail.com>
2024-01-30 14:19:37 +0900

    java-configuration-with-dependency-injection MainConfig를 Sender/Service config로 분리

---
+++ src/main/java/com/nhnacademy/edu/springframework/config/SenderConfig.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.edu.springframework.config;
+
+import com.nhnacademy.edu.springframework.sender.EmailMessageSender;
+import com.nhnacademy.edu.springframework.sender.MessageSender;
+import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
+import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:/beans.xml")
+public class SenderConfig {
+	@Bean("smsMessageSender")
+	public MessageSender smsMessageSender() {
+		return new SmsMessageSender();
+	}
+	@Bean("emailMessageSender")
+	public MessageSender emailMessageSender() {
+		return new EmailMessageSender();
+	}
+}
--- src/main/java/com/nhnacademy/edu/springframework/config/ServiceConfig.java
+++ src/main/java/com/nhnacademy/edu/springframework/config/ServiceConfig.java
@@ -1,27 +1,19 @@
 package com.nhnacademy.edu.springframework.config;
 
-import com.nhnacademy.edu.springframework.sender.EmailMessageSender;
 import com.nhnacademy.edu.springframework.sender.MessageSender;
-import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource("classpath:/beans.xml")
-public class MainConfig {
+public class ServiceConfig {
+	@Autowired
+	private SenderConfig senderConfig;
 	@Bean
 	public MessageSendService messageSendService(){
-		return new MessageSendService(smsMessageSender());
-	}
-
-	@Bean("smsMessageSender")
-	public MessageSender smsMessageSender() {
-		return new SmsMessageSender();
-	}
-	@Bean("emailMessageSender")
-	public MessageSender emailMessageSender() {
-		return new EmailMessageSender();
+		return new MessageSendService(senderConfig.smsMessageSender());
 	}
 }
