a9f999ac64806b5466dd8ad9bd2a595af8139e93
siddltkfkd <unji2000@gmail.com>
2024-01-30 15:03:56 +0900

    java-configuration-with-dependency-injection
    beans.xml 삭제
    @ComponentScan으로 교체

--- src/main/java/com/nhnacademy/edu/springframework/config/ServiceConfig.java
+++ src/main/java/com/nhnacademy/edu/springframework/config/ServiceConfig.java
@@ -4,11 +4,12 @@ import com.nhnacademy.edu.springframework.sender.MessageSender;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("classpath:/beans.xml")
+@ComponentScan(basePackages = "com.nhnacademy.edu.springframework")
 public class ServiceConfig {
 	@Autowired
 	private SenderConfig senderConfig;
