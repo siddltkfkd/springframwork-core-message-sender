88ff3a83a3bcd62634bb49bb1a7fa3146ef61ebe
siddltkfkd <unji2000@gmail.com>
2024-01-30 13:02:04 +0900

    java configuration

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -2,14 +2,14 @@ package com.nhnacademy.edu.springframework;
 
 import com.nhnacademy.edu.springframework.domain.User;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	public static void main(String[] args) {
-		try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")){
-			User user = new User("email@naver.com", "010-xxxx-xxxx");
-			MessageSendService service = context.getBean("messageSendService", MessageSendService.class);
-			service.doSendMessage(user, "hi");
-		}
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework");
+		User user = new User("email@naver.com", "010-xxxx-xxxx");
+		MessageSendService service = context.getBean("messageSendService", MessageSendService.class);
+		service.doSendMessage(user, "hi");
 	}
 }
