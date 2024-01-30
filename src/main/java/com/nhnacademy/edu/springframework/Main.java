0fa85b9f73028c3e21a497f1d8f530c12a731222
siddltkfkd <unji2000@gmail.com>
2024-01-30 13:14:35 +0900

    java configuration 주석 추가

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -7,6 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	public static void main(String[] args) {
+		// 패키지명은 상위만 간단하게 써도 된다. MainConfig.class라고 써도 잘 동작함
+		// 패키지 이름을 시작하는 하위 패키지들을 다 검색함
+		// -> 시간이 오래 걸릴 수 있다.
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework");
 		User user = new User("email@naver.com", "010-xxxx-xxxx");
 		MessageSendService service = context.getBean("messageSendService", MessageSendService.class);
